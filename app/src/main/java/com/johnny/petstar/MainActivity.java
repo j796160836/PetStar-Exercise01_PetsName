ae8747ef967b0c40de51c7310193f6ec0be2b2f1
Johnny <j796160836@gmail.com>
2015-01-07 15:35:34 +0800

    Exercise 01: Show pet’s name

--- app/src/main/java/com/johnny/petstar/MainActivity.java
+++ app/src/main/java/com/johnny/petstar/MainActivity.java
@@ -4,14 +4,29 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
+
+    private TextView pet_name;
+    private Button show_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pet_name = (TextView) findViewById(R.id.pet_name);
+        show_button = (Button) findViewById(R.id.show_button);
+        show_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v==show_button) {
+                    pet_name.setText("Pet's name: Eason.");
+                }
+            }
+        });
     }
 
 
